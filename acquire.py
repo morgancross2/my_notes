cc7a4167b1cd6dc774792c2487fa2e133fc189eb
Morgan Cross <morgan.e.cross2@gmail.com>
2022-08-08 13:59:49 -0500

    copies from exercises to use in note taking

---
+++ acquire.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import env
+import os
+
+def conn(db):
+    '''
+    This function uses my info from my env file to create a 
+    connection url to access the MySQL CodeUp database.
+    '''
+    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
+
+def get_titanic_data():
+    '''
+    This function checks if the titanic data is saved locally. 
+    If it is not local, this function reads the titanic data from 
+    the CodeUp MySQL database and return it in a DataFrame.
+    '''
+    filename = 'titanic.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        df = pd.read_sql('''SELECT 
+                            * FROM passengers;''', conn('titanic_db'))
+        df.to_csv(filename)
+        return df
+
+def get_iris_data():
+    '''
+    This function checks if the iris data is saved locally. 
+    If it is not local, this function reads the titanic data from 
+    the CodeUp MySQL database and return it in a DataFrame.
+    '''
+    filename = 'iris.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        df = pd.read_sql('''SELECT * 
+                            FROM measurements 
+                            JOIN species 
+                                USING(species_id);''', conn('iris_db'))
+        df.to_csv(filename)
+        return df
+
+def get_telco_data():
+    '''
+    This function checks if the telco data is saved locally. 
+    If it is not local, this function reads the titanic data from 
+    the CodeUp MySQL database and return it in a DataFrame.    '''
+    filename = 'telco.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        query = '''
+            SELECT * FROM customers
+            JOIN contract_types 
+                USING (contract_type_id)
+            JOIN payment_types 
+                USING (payment_type_id)
+            JOIN internet_service_types 
+                USING (internet_service_type_id);  
+        '''
+        df = pd.read_sql(query, conn('telco_churn')) 
+        df.to_csv(filename)
+        return df
+
---
+++ prepare.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+import os
+import acquire
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+
+def prep_iris(df):
+    '''
+    This function accepts the iris df and preps it. Returns train, validate, and test datasets. 
+    '''
+    df = df.drop(columns=['species_id', 'measurement_id'])
+    df = df.rename(columns={'species_name':'species'})
+    dummies = pd.get_dummies(df.species)
+    df = pd.concat([df, dummies], axis=1)
+    df = df.drop(columns='species')
+    
+    # no target to stratify 
+    train, test = train_test_split(df, test_size = .2, random_state=123)
+    train, validate = train_test_split(train, test_size = .25, random_state=123)
+    return train, validate, test
+
+
+def prep_titanic(df):
+    '''
+    This function accepts the titanic df and preps it. Returns train, validate, and test datasets. 
+    '''
+    df = df.drop(columns='embarked')
+    df = df.drop(columns='class')
+    df = df.drop(columns=['age','deck'])
+    dummy_df = pd.get_dummies(df[['sex']], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+    df = df.drop(columns=['sex'])
+    
+    # split data
+    train, val, test = split_data(df, 'survived')
+
+    # account for embark_town nulls using most frequent in train set
+    train, val, test = impute_mode(train, val, test, 'embark_town')
+    
+    # get_dummies for embark town columns
+    dummy_df = pd.get_dummies(train[['embark_town']])
+    train = pd.concat([train, dummy_df], axis=1)
+    train = train.drop(columns=['embark_town'])
+    dummy_df = pd.get_dummies(val[['embark_town']])
+    val = pd.concat([val, dummy_df], axis=1)
+    val = val.drop(columns=['embark_town'])
+    dummy_df = pd.get_dummies(test[['embark_town']])
+    test = pd.concat([test, dummy_df], axis=1)
+    test = test.drop(columns=['embark_town'])
+    
+    return train, val, test
+
+
+def prep_telco(telco):
+    '''
+    This function accepts the telco df and preps it. Returns train, validate, and test datasets. 
+    '''
+    # fix monthly charges from string to floats
+    telco.total_charges = telco.total_charges.str.replace(' ','0')
+    telco.total_charges = telco.total_charges.astype('float')
+    
+    # drop foreign key columns
+    telco = telco.drop(columns=['internet_service_type_id', 'payment_type_id', 'contract_type_id'])
+    
+    # split out 0 vs 1 columns
+    dummies = pd.get_dummies(telco[['gender','partner', 'dependents', 'phone_service','paperless_billing', 'churn']], drop_first=True)
+    telco = pd.concat([telco, dummies], axis=1)
+    telco = telco.drop(columns=['gender', 'partner', 'dependents', 'phone_service','paperless_billing', 'churn'])
+    
+    # split out category columns
+    dum = pd.get_dummies(telco.contract_type)
+    telco = pd.concat([telco, dum], axis=1)
+    telco = telco.drop(columns='contract_type')
+    dum = pd.get_dummies(telco.internet_service_type)
+    telco = pd.concat([telco, dum[['DSL', 'Fiber optic']]], axis=1)
+    dum = pd.get_dummies(telco.payment_type)
+    telco = pd.concat([telco, dum], axis=1)
+    telco = telco.drop(columns=(['payment_type', 'internet_service_type']))
+    
+    # relabel final columns with 0 or 1 values
+    telco.multiple_lines = telco.multiple_lines.str.replace('No phone service', '0').str.replace('Yes', '2').str.replace('No', '1').astype('int')
+    telco.online_security = telco.online_security.str.replace('No internet service', '0').str.replace('Yes', '1').str.replace('No', '0').astype('int')
+    telco.online_backup = telco.online_backup.str.replace('No internet service', '0').str.replace('Yes', '1').str.replace('No', '0').astype('int')
+    telco.device_protection = telco.device_protection.str.replace('No internet service', '0').str.replace('Yes', '1').str.replace('No', '0').astype('int')
+    telco.tech_support = telco.tech_support.str.replace('No internet service', '0').str.replace('Yes', '1').str.replace('No', '0').astype('int')
+    telco.streaming_tv = telco.streaming_tv.str.replace('No internet service', '0').str.replace('Yes', '1').str.replace('No', '0').astype('int')
+    telco.streaming_movies = telco.streaming_movies.str.replace('No internet service', '0').str.replace('Yes', '2').str.replace('No', '0').astype('int')
+    
+    return split_data(telco, 'churn_Yes')
+
+
+def split_data(df, target):
+    '''
+    Takes in a dataframe and target column (as a string) and returns train, validate, 
+    and test subset dataframes with the .2/.8 and .25/.75 splits to create a final
+    .2/.2/.6 split between datasets
+    '''
+    train, test = train_test_split(df, test_size = .2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train, test_size = .25, random_state=123, stratify=train[target])
+    
+    return train, validate, test
+
+
+def impute_mode(train, validate, test, col):
+    '''
+    Takes in train, validate, and test as dfs, and column name (as string) and uses train 
+    to identify the best value to replace nulls in embark_town
+    
+    Imputes the most_frequent value into all three sets and returns all three sets
+    '''
+    imputer = SimpleImputer(strategy='most_frequent')
+    imputer = imputer.fit(train[[col]])
+    train[[col]] = imputer.transform(train[[col]])
+    validate[[col]] = imputer.transform(validate[[col]])
+    test[[col]] = imputer.transform(test[[col]])
+    
+    return train, validate, test
